0bfa1fded75d6419fe6877e97e5bf836f45eecac
daan de groot <daan.de.groot4@hva.nl>
2018-04-24 14:08:51 +0200

    automate contols gemaakt

--- src/autopilot.py
+++ src/autopilot.py
@@ -9,8 +9,38 @@ motor_l = Motor(16, 18, 12)
 
 target_direction = get_target()
 
+
+def rotate():
+    spd = 100
+
+    if target_direction == 0:
+        motor_l.drive("r", spd)
+        motor_r.drive("f", spd)
+
+    elif target_direction == 1:
+        motor_l.drive("f", spd)
+        motor_r.drive("r", spd)
+
+    elif target_direction == 0.5:
+        motor_l.drive("f", spd)
+        motor_r.drive("f", spd)
+
+    elif target_direction == -1:
+        motor_l.drive("f", spd)
+        motor_r.drive("r", spd)
+
+
+def shut_down():
+    spd = 0
+
+    if battery.get_spanning() == 3.70:
+        motor_l.brake(spd)
+        motor_r.brake(spd)
+
 # go to target
 
 # if no target is found go look for one
 
 # if battery is empty shutdown
+
+# 3.6-4.2 voor battery
--- src/motion_detection.py
+++ src/motion_detection.py
@@ -3,4 +3,4 @@ def get_target():
     Vind beweging in het zichtveld van de camera. 0 voor links en 1 voor rechts. -1 voor geen beweging.
     :return: De richting waar zich de beweging bevind,
     """
-    return 0.3
+    return 0.5
